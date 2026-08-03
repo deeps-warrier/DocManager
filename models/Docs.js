c9baa8f8a9e67180b648b4cd2f44e3db830c18ee
deeps-warrier <deeps.sunrise@gmail.com>
2026-08-03 10:04:31 +0530

    Update Docs.js

--- models/Docs.js
+++ models/Docs.js
@@ -1,30 +1,44 @@
 const mongoose = require('mongoose');
 
 const docSchema = new mongoose.Schema({
+
     department: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Department',
         required: true
     },
+
     category: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Category',
         required: true
     },
+
     files: [{
+
         originalname: String,
+
         filename: String,
+
         path: String,
+
+        cloudinaryUrl: String,
+
+        publicId: String,
+
         uploadDate: {
             type: Date,
             default: Date.now
         }
+
     }],
+
     uploadedBy: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true
     }
+
 });
 
 module.exports = mongoose.model('Doc', docSchema);
